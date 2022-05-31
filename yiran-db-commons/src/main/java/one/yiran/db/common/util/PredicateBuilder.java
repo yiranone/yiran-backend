a022fee0f22e7c534477aa7d740f28227a5bf414
jimmy <jimmy@izhong>
2022-05-31 14:37:41 +0800

    优化筛选逻辑

--- yiran-db-commons/src/main/java/one/yiran/db/common/util/PredicateBuilder.java
+++ yiran-db-commons/src/main/java/one/yiran/db/common/util/PredicateBuilder.java
@@ -23,7 +23,7 @@ import java.util.List;
 @Slf4j
 public class PredicateBuilder {
 
-    public static Builder builder(){
+    public static Builder builder() {
         return new Builder();
     }
 
@@ -42,26 +42,27 @@ public class PredicateBuilder {
         private List<PredicateBox> boxes = new ArrayList<>();
         private List<Predicate> predicates = new ArrayList<>();
 
-        public Builder addGreaterOrEqualIfNotBlank(Path path, Object value){
-            if(value != null)
-                boxes.add(new PredicateBox(path,Ops.GOE,value,false));
+        public Builder addGreaterOrEqualIfNotBlank(Path path, Object value) {
+            if (value != null)
+                boxes.add(new PredicateBox(path, Ops.GOE, value, false));
             return this;
         }
 
-        public Builder addLittlerOrEqualIfNotBlank(Path path, Object value){
-            if(value != null)
-                boxes.add(new PredicateBox(path,Ops.LOE,value,false));
+        public Builder addLittlerOrEqualIfNotBlank(Path path, Object value) {
+            if (value != null)
+                boxes.add(new PredicateBox(path, Ops.LOE, value, false));
             return this;
         }
 
-         public Builder addEqualIfNotBlank(Path path, Object value){
-            if(value != null)
-                boxes.add(new PredicateBox(path,Ops.EQ,value,false));
-             return this;
-         }
-        public Builder addLikeIfNotBlank(StringPath path, String value){
-            if(StringUtils.isNotBlank(value))
-                boxes.add(new PredicateBox(path,Ops.LIKE, "%" + value + "%",false));
+        public Builder addEqualIfNotBlank(Path path, Object value) {
+            if (value != null)
+                boxes.add(new PredicateBox(path, Ops.EQ, value, false));
+            return this;
+        }
+
+        public Builder addLikeIfNotBlank(StringPath path, String value) {
+            if (StringUtils.isNotBlank(value))
+                boxes.add(new PredicateBox(path, Ops.LIKE, "%" + value + "%", false));
             return this;
         }
 
@@ -102,7 +103,7 @@ public class PredicateBuilder {
                         }
 
                         if (op.equals(Search.Op.IS)) {
-                            if(key.equals("isDelete")) {
+                            if (key.equals("isDelete")) {
                                 if (value instanceof Boolean) {
                                     if (((Boolean) value).booleanValue()) {
                                         predicates.add(PredicateUtil.buildDeletePredicate(path));
@@ -112,18 +113,18 @@ public class PredicateBuilder {
                                     }
                                 }
                             } else {
-                                boxes.add(new PredicateBox(pathValue,Ops.EQ,value,false));
+                                boxes.add(new PredicateBox(pathValue, Ops.EQ, value, false));
                             }
                         } else if (op.equals(Search.Op.REGEX)) {
-                            boxes.add(new PredicateBox(pathValue ,Ops.LIKE,value,false));
+                            boxes.add(new PredicateBox(pathValue, Ops.LIKE, value, false));
                         } else if (op.equals(Search.Op.IN)) {
-                            boxes.add(new PredicateBox(pathValue,Ops.IN,value,false));
+                            boxes.add(new PredicateBox(pathValue, Ops.IN, value, false));
                         } else if (op.equals(Search.Op.GT)) {
-                            boxes.add(new PredicateBox(pathValue,Ops.GT,value,false));
+                            boxes.add(new PredicateBox(pathValue, Ops.GT, value, false));
                         } else if (op.equals(Search.Op.GTE)) {
-                            boxes.add(new PredicateBox(pathValue,Ops.GOE,value,false));
+                            boxes.add(new PredicateBox(pathValue, Ops.GOE, value, false));
                         } else if (op.equals(Search.Op.LT)) {
-                            boxes.add(new PredicateBox(pathValue,Ops.LOE,value,false));
+                            boxes.add(new PredicateBox(pathValue, Ops.LOE, value, false));
                         }
                     }
                 }
@@ -131,22 +132,25 @@ public class PredicateBuilder {
             return this;
         }
 
-         public List<Predicate> toList() {
-            return convert();
-        }
-        public Predicate[] toArray() {
-            return convert().toArray(new Predicate[]{});
+        public List<Predicate> toList() {
+            List<Predicate> cvs = convert();
+            cvs.addAll(predicates);
+            return cvs;
         }
 
-        private List<Predicate> convert(){
+        public Predicate[] toArray() {
+            return toList().toArray(new Predicate[]{});
+        }
+
+        private List<Predicate> convert() {
             List<Predicate> list = new ArrayList<>();
-            for(PredicateBox b: boxes) {
+            for (PredicateBox b : boxes) {
                 Path path = b.getPath();
                 Ops ops = b.getOps();
                 Object value = b.getValue();
                 boolean allowNull = b.isAllowNull();
                 if (value != null) {
-                    if (ops ==  Ops.GOE) {
+                    if (ops == Ops.GOE) {
                         if (path instanceof DateExpression && value instanceof LocalDate) {
                             list.add(((DateExpression) path).goe((LocalDate) value));
                         } else if (path instanceof NumberExpression && value instanceof Number) {
@@ -155,16 +159,16 @@ public class PredicateBuilder {
                             log.error("Predicate构建异常 path={},ops={} value{}", path, ops, value);
                             throw new RuntimeException("Predicate构建异常");
                         }
-                    } else if(ops == Ops.LOE) {
+                    } else if (ops == Ops.LOE) {
                         if (path instanceof DateExpression && value instanceof LocalDate) {
-                            list.add(((DateExpression)path).loe((LocalDate) value));
-                        } else if(path instanceof NumberExpression && value instanceof Number) {
-                            list.add(((NumberExpression)path).loe((Number)value));
+                            list.add(((DateExpression) path).loe((LocalDate) value));
+                        } else if (path instanceof NumberExpression && value instanceof Number) {
+                            list.add(((NumberExpression) path).loe((Number) value));
                         } else {
                             log.error("Predicate构建异常 path={},ops={} value{}", path, ops, value);
                             throw new RuntimeException("Predicate构建异常");
                         }
-                    } if (ops ==  Ops.GT) {
+                    } else if (ops == Ops.GT) {
                         if (path instanceof DateExpression && value instanceof LocalDate) {
                             list.add(((DateExpression) path).gt((LocalDate) value));
                         } else if (path instanceof NumberExpression && value instanceof Number) {
@@ -173,40 +177,40 @@ public class PredicateBuilder {
                             log.error("Predicate构建异常 path={},ops={} value{}", path, ops, value);
                             throw new RuntimeException("Predicate构建异常");
                         }
-                    } else if(ops == Ops.LT) {
+                    } else if (ops == Ops.LT) {
                         if (path instanceof DateExpression && value instanceof LocalDate) {
-                            list.add(((DateExpression)path).lt((LocalDate) value));
-                        } else if(path instanceof NumberExpression && value instanceof Number) {
-                            list.add(((NumberExpression)path).lt((Number)value));
-                        } else {
-                            log.error("Predicate构建异常 path={},ops={} value{}", path, ops, value);
-                            throw new RuntimeException("Predicate构建异常");
-                        }
-                    } else if(ops == Ops.EQ) {
-                        if (path instanceof SimpleExpression && value instanceof String) {
-                            list.add(((SimpleExpression)path).eq((String) value));
+                            list.add(((DateExpression) path).lt((LocalDate) value));
                         } else if (path instanceof NumberExpression && value instanceof Number) {
-                            list.add(((NumberExpression)path).eq((Number) value));
-                        } else if(path instanceof BooleanExpression && value instanceof Boolean) {
-                            list.add(((BooleanExpression)path).eq((Boolean) value));
+                            list.add(((NumberExpression) path).lt((Number) value));
                         } else {
                             log.error("Predicate构建异常 path={},ops={} value{}", path, ops, value);
                             throw new RuntimeException("Predicate构建异常");
                         }
-                    } else if(ops == Ops.IN) {
-                        if(value instanceof Number[]) {
-                            Number[] numValue = (Number[]) value;
-                            if(numValue.length > 0)
-                                list.add(((SimpleExpression)path).in(numValue));
-                        }else if(value instanceof String[]) {
-                            String[] strValue = (String[]) value;
-                            if(strValue.length > 0)
-                                list.add(((SimpleExpression)path).in(strValue));
+                    } else if (ops == Ops.EQ) {
+                        if (path instanceof SimpleExpression && value instanceof String) {
+                            list.add(((SimpleExpression) path).eq((String) value));
+                        } else if (path instanceof NumberExpression && value instanceof Number) {
+                            list.add(((NumberExpression) path).eq((Number) value));
+                        } else if (path instanceof BooleanExpression && value instanceof Boolean) {
+                            list.add(((BooleanExpression) path).eq((Boolean) value));
+                        } else {
+                            log.error("Predicate构建异常 path={},ops={} value{}", path, ops, value);
+                            throw new RuntimeException("Predicate构建异常");
                         }
-                    } else if(ops == Ops.LIKE) {
+                    } else if (ops == Ops.IN) {
+                        if (value instanceof Number[]) {
+                            Number[] numValue = (Number[]) value;
+                            if (numValue.length > 0)
+                                list.add(((SimpleExpression) path).in(numValue));
+                        } else if (value instanceof String[]) {
+                            String[] strValue = (String[]) value;
+                            if (strValue.length > 0)
+                                list.add(((SimpleExpression) path).in(strValue));
+                        }
+                    } else if (ops == Ops.LIKE) {
                         if (path instanceof StringExpression && value instanceof String) {
-                            if(StringUtils.isNotBlank((String) value))
-                                list.add(((StringExpression)path).like((String) value));
+                            if (StringUtils.isNotBlank((String) value))
+                                list.add(((StringExpression) path).like((String) value));
                         }
                     } else {
                         log.error("ops还不支持 path={},ops={} value{}", path, ops, value);
