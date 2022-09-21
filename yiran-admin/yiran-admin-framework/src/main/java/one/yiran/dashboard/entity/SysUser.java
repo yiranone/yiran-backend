2748698b017fe828f4e973bf35384c400ca26a06
jimmy <jimmy@izhong>
2022-09-21 19:17:21 +0800

    fix

--- yiran-admin/yiran-admin-framework/src/main/java/one/yiran/dashboard/entity/SysUser.java
+++ yiran-admin/yiran-admin-framework/src/main/java/one/yiran/dashboard/entity/SysUser.java
@@ -54,7 +54,7 @@ public class SysUser extends TimedBasedEntity {
 
     @Excel(name = "手机号码")
     @Search
-    @Size(min = 0, max = 11, message = "手机号码长度不能超过11个字符")
+    @Size(min = 0, max = 32, message = "手机号码长度不能超过32个字符")
     @Column
     private String phoneNumber;
 
